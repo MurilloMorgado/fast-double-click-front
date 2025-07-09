22df1d327edfd6d373a640833752336cbb350b83
MurilloMorgado <mumorgado@hotmail.com>
2025-07-09 16:07:47 -0300

    implementando o component que vai receber a tabela de resultados

--- fast-double-click-front/src/app/app.routes.ts
+++ fast-double-click-front/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { ResultadoComponent } from './views/resultado/resultado.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'resultado', component: ResultadoComponent }
 ];
