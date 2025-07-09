5bc2534f321a1cbbce4b3e047860f6909a86a118
MurilloMorgado <mumorgado@hotmail.com>
2025-07-09 20:01:24 -0300

    aplicando filtro de intervalo de datas

--- fast-double-click-front/src/app/views/resultado/resultado.component.ts
+++ fast-double-click-front/src/app/views/resultado/resultado.component.ts
@@ -8,18 +8,26 @@ import { FooterComponent } from "../../components/footer/footer.component";
 import { HeaderComponent } from "../../components/header/header.component";
 import { RegistroTempo } from '../../model/registroTempo';
 import { CronometroService } from '../../service/cronometroService.service';
+import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
+import { log } from 'console';
 
 
 @Component({
   selector: 'app-resultado',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, TableModule, InputTextModule, IconFieldModule, InputIconModule],
+  imports: [HeaderComponent, FooterComponent, TableModule, InputTextModule, IconFieldModule, InputIconModule, CalendarModule, FormsModule],
   templateUrl: './resultado.component.html',
   styleUrl: './resultado.component.css'
 })
 export class ResultadoComponent implements OnInit {
 
   registrotempo!: RegistroTempo[];
+  registrotempoFiltrado: RegistroTempo[] = [];
+
+  rangeDates: Date[] = [];
+  dataInicio!: any;
+  dataFim!: any;
 
   constructor(
     private cronometroService: CronometroService
@@ -32,7 +40,39 @@ export class ResultadoComponent implements OnInit {
 
   async listarResultados() {
     this.registrotempo = await firstValueFrom(this.cronometroService.listarTempos());
+    this.registrotempoFiltrado = [...this.registrotempo];
     console.log(this.registrotempo);
   }
+
+  filtrarPorData(): void {
+    const [inicio, fim] = this.rangeDates;
+
+    if (!inicio || !fim) {
+      this.registrotempoFiltrado = [...this.registrotempo];
+      return;
+    }
+
+  const dataInicioStr = this.formatDate(inicio);
+  const dataFimStr = this.formatDate(fim);
+
+  console.log(dataInicioStr);
+  console.log(dataFimStr);
+  this.registrotempoFiltrado = this.registrotempo.filter((registro) => {
+    return registro.data >= dataInicioStr && registro.data <= dataFimStr;
+  });
+  console.log(this.registrotempoFiltrado);
+
+  }
+
+  limparFiltro(): void {
+  this.rangeDates = [];
+  this.registrotempoFiltrado = [...this.registrotempo];
+}
+
+
+formatDate(date: Date): string {
+  return date.toISOString().split('T')[0];
+}
+
 
 }
