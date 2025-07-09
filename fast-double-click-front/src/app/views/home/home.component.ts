20c019fbf62ac1ec2b3e78c918a5c3050d82d640
MurilloMorgado <mumorgado@hotmail.com>
2025-07-09 15:38:51 -0300

    resolvendo problema de versao e importacoes

--- fast-double-click-front/src/app/views/home/home.component.ts
+++ fast-double-click-front/src/app/views/home/home.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MenubarModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit(): void {
+    this.items = [
+      { label: 'Home', icon: 'pi pi-home' },
+      { label: 'Sobre', icon: 'pi pi-info-circle' }
+    ];
+
+  }
 
 }
